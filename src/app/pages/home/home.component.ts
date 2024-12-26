0ca1620e9ed601bf3bd9df15a845075fe4902170
cpminipc <cpierres@hotmail.com>
2024-12-26 10:42:14 +0100

    step6 : refactoring composant olympic-global-graph.component avec @input

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,8 +1,9 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {LoadingService} from "../../core/services/loading.service";
-import {Observable} from "rxjs";
+import {Observable, of, take} from "rxjs";
 import {MedalPieData} from "../../core/models/stats/MedalPieData";
 import {OlympicService} from "../../core/services/olympic.service";
+import {Olympic} from "../../core/models/Olympic";
 
 @Component({
   selector: 'app-home',
@@ -29,16 +30,24 @@ export class HomeComponent implements OnInit {
   constructor(private olympicService: OlympicService, private loadingService: LoadingService) {
     console.log('home.component.ts constructor()');
     console.log('LoadingService instance (from home.component) :', this.loadingService);
-    this.loadingService.loadingOn();
   }
 
   ngOnInit(): void {
     console.log('home.component.ts ngOnInit()');
-    //this.olympics$ = this.olympicService.getOlympics();
+    // this.loadingService.loadingOn();//mauvaise solution je sais : juste pour test ... mais KO aussi
+    // setTimeout(() => {
+    //   console.log('Attente terminée, continuer...');
+    // }, 2000); // Attend 2 secondes
+    // this.olympics$ = this.olympicService.loadInitialData().pipe(
+    //   //switchMap pour ne garder qu'un flux rempli et éviter l'affichage des 0 avant vraies données sur réseau lent
+    //   //switchMap(() => this.subject.pipe(filter(data => !!data && data.length > 0))),
+    //   take(1)// Prend juste la 1ère donnée et se complète automatiquement (=> unsubscribe)
+    //   // On met take(1) ici et non pas dans loadInitialData pour plus de flexibilité
+    // ).subscribe((data: Olympic[]) => console.log('OlympicService.refreshDataCache() ; Data reçues dans subscribe : ', data));
 
     this.participationStats$ = this.olympicService.getParticipationStats();
     this.medalPieData$ = this.olympicService.getMedalsPieData();
-    this.loadingService.loadingOff();
+    //this.loadingService.loadingOff();//mauvaise solution je sais : juste pour test ... mais KO aussi
   }
 
 }
